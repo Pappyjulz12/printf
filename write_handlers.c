075f1e463790a3170b3400da92829a126efa73a9
Victoria Edeh <106410493+Veedymphna@users.noreply.github.com>
2023-07-23 18:19:37 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -3,13 +3,13 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - it prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
+ * @flags:  it calculates active flags.
  * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
+ * @precision: the precision specifier
+ * @size: the size specifier
  *
  * Return: Number of chars printed.
  */
@@ -47,14 +47,14 @@ int handle_write_char(char c, char buffer[],
 
 /************************* WRITE NUMBER *************************/
 /**
- * write_number - Prints a string
- * @is_negative: Lista of arguments
+ * write_number - it prints a string
+ * @is_negative: List a of arguments
  * @ind: char types.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  it calculates active flags
  * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
+ * @precision: the precision specifier
+ * @size: the size specifier
  *
  * Return: Number of chars printed.
  */
@@ -80,12 +80,12 @@ int write_number(int is_negative, int ind, char buffer[],
 }
 
 /**
- * write_num - Write a number using a bufffer
+ * write_num - it writes a number using a bufffer
  * @ind: Index at which the number starts on the buffer
  * @buffer: Buffer
  * @flags: Flags
  * @width: width
- * @prec: Precision specifier
+ * @prec: the precision specifier
  * @length: Number length
  * @padd: Pading char
  * @extra_c: Extra char
@@ -139,14 +139,14 @@ int write_num(int ind, char buffer[],
 }
 
 /**
- * write_unsgnd - Writes an unsigned number
+ * write_unsgnd - it writes an unsigned number
  * @is_negative: Number indicating if the num is negative
  * @ind: Index at which the number starts in the buffer
  * @buffer: Array of chars
- * @flags: Flags specifiers
- * @width: Width specifier
- * @precision: Precision specifier
- * @size: Size specifier
+ * @flags: the flags specifiers
+ * @width: the width specifier
+ * @precision: the precision specifier
+ * @size: the size specifier
  *
  * Return: Number of written chars.
  */
@@ -197,7 +197,7 @@ int write_unsgnd(int is_negative, int ind,
 }
 
 /**
- * write_pointer - Write a memory address
+ * write_pointer - it write a memory address
  * @buffer: Arrays of chars
  * @ind: Index at which the number starts in the buffer
  * @length: Length of number
